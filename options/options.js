cfe2d1072b1c6f7532a2aac5800d33da91b02a4d
Crowdbotics <team@crowdbotics.com>
2024-02-05 17:24:33 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "backupdisasterrecov-45937",
+  url: "https://backupdisasterrecov-45937.botics.co",
+  api: "https://backupdisasterrecov-45937.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
